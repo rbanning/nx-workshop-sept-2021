0ddb6a226319714b0911cde1b9775b917f3f89e3
rbanning <rob@myhallpass.com>
2021-09-14 10:49:31 -0700

    lab 5

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+
+import { formatRating } from "@bg-hoard/store/util-formatters";
+
 import { getAllGames } from '../fake-api';
 
 @Component({
@@ -9,4 +12,5 @@ import { getAllGames } from '../fake-api';
 export class AppComponent {
   title = 'Board Game Hoard';
   games = getAllGames();
+  formatRating = formatRating;
 }

93ad2e3172ba1f6ff8fdd18a20a26b9ba00d1a44
rbanning <rob@myhallpass.com>
2021-09-15 09:27:26 -0700

    update lab 13

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,8 +1,21 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
+
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  const PREFIX = "util-";
+  let { name } = schema;
+  if (name?.indexOf(PREFIX) !== 0) {
+    name = PREFIX + name;
+  }
+
+  //add scope tag
+  const tag = `scope:${schema.directory}, type:util`;
+  schema.tags = !!schema.tags ? `${schema.tags}, ${tag}` : tag;
+
+  console.log("DEBUG: -- ", {name, schema});
+
+  await libraryGenerator(tree, schema);
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);

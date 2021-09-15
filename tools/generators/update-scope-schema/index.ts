6eb33159dd77f42d80f5ac53203aee7831e77a81
rbanning <rob@myhallpass.com>
2021-09-15 10:27:26 -0700

    lab 14b

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -1,10 +1,63 @@
-import { Tree, formatFiles, updateJson } from '@nrwl/devkit';
+import { Tree, formatFiles, updateJson, readJson } from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  updateJson(tree, "workspace.json", (json) => {
-    json.defaultProject = 'api';
+function getScopes(nxJson: any): string[] {
+  const TARGET = 'scope:';
+  const projects: any[] = Object.values(nxJson.projects);
+  const allScopes = projects
+    .map(project => project.tags
+      .filter((tag: string) => tag.startsWith(TARGET))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])         //convert to array of strings (from array of arrays) - i.e. flatten array
+    .map((scope: string) => scope.slice(TARGET.length));  //remove the prefix (TARGET)
+  return Array.from(new Set(allScopes));                  //remove duplicates
+}
+
+//replaces schema interface's list of directory options (scopes)
+function updateInterfaceScopes(content: string, scopes: string[]): string {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const PATTERN = /interface ISchema \{\n.*\n.*\n.*\n\}/gm;
+  return content.replace(
+    PATTERN,
+    `interface ISchema {
+  name: string;
+  directory: ${joinScopes};
+  tags?: string;
+}`
+  );
+}
+
+
+function addScopeTagIfMissing(host: Tree) {
+  updateJson(host, 'nx.json', (json) => {
+    Object.keys(json.projects).forEach((projectName) => {
+      if (!json.projects[projectName].tags.some((tag) => tag.startsWith('scope:'))) {
+        const scope = projectName.split('-')[0];
+        json.projects[projectName].tags.push(`scope:${scope}`);
+      }
+    });
     return json;
   });
+}
 
+export default async function (tree: Tree, schema: any) {
+  //update scopes in util-lib
+  const scopes = getScopes(readJson(tree, "nx.json"));
+  updateJson(tree, 'tools/generators/util-lib/schema.json', schemaJson => {
+    schemaJson.properties.directory["x-prompt"].items = scopes.map(scope => ({
+      value: scope,
+      label: scope
+    }))
+    return schemaJson;
+  });
+
+  //add any missing scope tags to nx.json
+  await addScopeTagIfMissing(tree);
+
+  //update scopes in util-lib ISchema interface
+  const content = tree.read('tools/generators/util-lib/index.ts', 'utf-8');
+  const newContent = updateInterfaceScopes(content, scopes);
+  tree.write('tools/generators/util-lib/index.ts', newContent);
+
+  //finally
   await formatFiles(tree);  
 }
--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,19 +1,23 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
+interface ISchema {
+  name: string;
+  directory: 'store' | 'shared';
+  tags?: string;
+}
 
-export default async function (tree: Tree, schema: any) {
-  const PREFIX = "util-";
-  let { name } = schema;
-  if (name?.indexOf(PREFIX) !== 0) {
-    name = PREFIX + name;
+export default async function (tree: Tree, schema: ISchema) {
+  const PREFIX = 'util-';
+  if (schema.name && schema.name.indexOf(PREFIX) !== 0) {
+    schema.name = PREFIX + schema.name;
   }
 
   //add scope tag
   const tag = `scope:${schema.directory}, type:util`;
   schema.tags = !!schema.tags ? `${schema.tags}, ${tag}` : tag;
 
-  console.log("DEBUG: -- ", {name, schema});
+  console.log('DEBUG: -- ', { name, schema });
 
   await libraryGenerator(tree, schema);
   await formatFiles(tree);
